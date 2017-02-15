3f19a7a1a4a6b0bc9180801dc63a4fbfc6d73bd1
Robson Gian Perassoli <robsonperassoli@gmail.com>
2017-02-15 10:54:20 -0200

    Finish identicon exercise and setup the vagrant machine to provision phoenix

--- provision.sh
+++ provision.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
+#Erlang and elixir
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb 
 dpkg -i erlang-solutions_1.0_all.deb
 apt-get update -y
 apt-get install -y esl-erlang
 apt-get install -y elixir
+
+# Phoenix
+mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez --force
+
+# Nvm and Node JS
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+source $HOME/.bashrc
+nvm install 6.5.0
+
+# PostgreSQL
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
+apt-get update -y
+apt-get install -y postgresql postgresql-contrib
+
+# inotify-tools
+apt-get install -y inotify-tools
