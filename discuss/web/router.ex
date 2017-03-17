ef3e98b5453569638e5a44bf27f4707a675dfaaa
Robson Gian Perassoli <robsonperassoli@gmail.com>
2017-03-16 23:47:25 -0300

    Add github OAuth authentication

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -7,6 +7,7 @@ defmodule Discuss.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Discuss.Plugs.SetUser
   end
 
   pipeline :api do
@@ -24,6 +25,16 @@ defmodule Discuss.Router do
     #delete "/topics/:id", TopicController, :delete
 
     resources "/", TopicController
+  end
+
+  scope "/auth", Discuss do
+    pipe_through :browser
+
+
+    get "/signout", AuthController, :signout
+    get "/:provider", AuthController, :request
+    get "/:provider/callback", AuthController, :callback
+
   end
 
   # Other scopes may use custom stacks.
